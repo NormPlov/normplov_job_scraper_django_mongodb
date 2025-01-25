a9fe7d75d42742bd740a43e3e62452d68e333118
lymann <phplaravel576@gmail.com>
2025-01-25 16:04:05 +0700

    change fast api url from ip to production

--- scraper/services/job_service.py
+++ scraper/services/job_service.py
@@ -878,7 +878,7 @@ class JobService:
             }
 
             headers = {"Authorization": token}
-            fastapi_url = "http://202.178.125.77:3300/api/v1/jobs"
+            fastapi_url = "https://normplov-api.istad.co/api/v1/jobs"
 
             # Send the request to FastAPI as form-data
             try:
